5ba31b618b5de000ced766963945b249e86ed748
mung96 <mung5079@gmail.com>
2025-06-26 09:56:58 +0900

    hook-form tut

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
 import "./App.css";
+import { useForm, SubmitHandler } from "react-hook-form";
 
-function App() {
-  return <div></div>;
+type Inputs = {
+  example: string;
+  exampleRequired: string;
+};
+
+export default function App() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  const res = () => {
+    return { onChange: () => console.log("change") };
+  };
+  console.log("렌더링");
+
+  watch("example"); //watch를 달면 제어로 변경됨
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* 이런 형태로 넘기는게 가능하구나 */}
+      <input {...res()} />
+      <input {...register("example")} />
+      <input {...register("exampleRequired", { required: true })} />
+      {errors.exampleRequired && <p>필수값입니다.</p>}
+
+      <input type="submit" />
+    </form>
+  );
 }
-
-export default App;
